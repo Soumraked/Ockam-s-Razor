b949cdfa3a950617d45968730303daa41e638d18
Danilo Ibáñez <danilobepo@gmail.com>
2020-11-28 16:49:32 -0300

    Fullscreen app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //Libraries
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 //Extensions
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       title: "Ockam's Razor",
       debugShowCheckedModeBanner: false,
