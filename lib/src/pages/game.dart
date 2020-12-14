26a59622e762df855f8dd8561870e9c714f82a33
RodrigoAlejandroBP <rbravop97@gmail.com>
2020-12-14 19:07:36 -0300

    cambiada la info del juego, solo falta el gif

--- lib/src/pages/game.dart
+++ lib/src/pages/game.dart
@@ -408,7 +408,7 @@ class _GameState extends State<Game> {
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.2,
         width: MediaQuery.of(context).size.width * 0.9,
-        child: Center(
+        child: Center(  
           child: AutoSizeText(
             '${_dialogos[0].dialogo}',
             textAlign: TextAlign.center,
